1d994235d6cdc82656698f7ec10cef9ee6686b6b
Sergio Raccoon Guasch <sergio@openfort.xyz>
2025-08-14 12:15:18 +0200

    chore: add passkey use validation

--- internal/adapters/handlers/rest/sharehdl/validator.go
+++ internal/adapters/handlers/rest/sharehdl/validator.go
@@ -9,6 +9,10 @@ func newValidator() *validator {
 	return &validator{}
 }
 
+func shareHasUserEntropyParams(share *Share) bool {
+	return share.Salt != "" || share.Iterations != 0 || share.Length != 0 || share.Digest != ""
+}
+
 func (v *validator) validateShare(share *Share) *api.Error {
 	if share.Secret == "" {
 		return api.ErrBadRequestWithMessage("secret is required")
@@ -39,17 +43,32 @@ func (v *validator) validateShare(share *Share) *api.Error {
 			return api.ErrBadRequestWithMessage("digest is required when entropy is user")
 		}
 	case EntropyProject:
-
 		if share.ShareStorageMethodID != StorageMethodShield {
 			return api.ErrBadRequestWithMessage("storage_method must be Shield if entropy is project")
 		}
 
-		if share.Salt != "" || share.Iterations != 0 || share.Length != 0 || share.Digest != "" {
+		if shareHasUserEntropyParams(share) {
 			return api.ErrBadRequestWithMessage("if user entropy is not set, encryption parameters should not be set")
 		}
 
 		if share.EncryptionPart == "" && share.EncryptionSession == "" {
 			return api.ErrBadRequestWithMessage("encryption_part or encryption_session is required when entropy is project")
+		}
+	case EntropyPasskey:
+		if share.ShareStorageMethodID != StorageMethodShield {
+			return api.ErrBadRequestWithMessage("storage_method must be Shield if entropy is passkey")
+		}
+
+		// User entroy parameters should not be set for passkey entropy
+		if shareHasUserEntropyParams(share) {
+			return api.ErrBadRequestWithMessage("if user entropy is not set, encryption parameters should not be set")
+		}
+
+		// Encryption part/encryption session belong to project entropy use case
+		// Here we're only storing an encrypted share and hinting how it's encrypted, like we're doing for user entropy
+		// Except we rely on proper user authentication for correct passkey retrieval and decryption
+		if share.EncryptionPart != "" || share.EncryptionSession != "" {
+			return api.ErrBadRequestWithMessage("encryption parameters should not be set for passkey entropy")
 		}
 	default:
 		return api.ErrBadRequestWithMessage("invalid entropy")
