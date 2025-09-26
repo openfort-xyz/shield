775d4b47f334a0932107508fdcb4944886a71b6f
n00m4d <grozinok@gmail.com>
2025-09-26 19:12:39 +0200

    chore: slightly change migration

--- internal/adapters/repositories/sql/migrations/20250910134804_project_2fa.sql
+++ internal/adapters/repositories/sql/migrations/20250910134804_project_2fa.sql
@@ -1,14 +1,13 @@
 -- +goose Up
--- +goose StatementBegin
+
 ALTER TABLE shld_projects
 ADD COLUMN enable_2fa BOOL NOT NULL DEFAULT false;
--- +goose StatementEnd
-
--- +goose StatementBegin
 UPDATE shld_projects SET enable_2fa = false;
+-- +goose StatementBegin
 -- +goose StatementEnd
 
 -- +goose Down
+
+    ALTER TABLE shld_projects DROP COLUMN IF EXISTS enable_2fa;
 -- +goose StatementBegin
-ALTER TABLE shld_projects DROP COLUMN IF EXISTS enable_2fa;
 -- +goose StatementEnd
